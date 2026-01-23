3f3443a09478765e5c4c5d2109827a561574b753
ashish0414 <ashishkrsingh91@gmail.com>
2026-01-23 18:09:07 +0530

    Handeled JS standard confirm prompt alerts

---
+++ tests/lambdaTestDemo/HandlingJavaScriptAlerts.spec.js
@@ -0,0 +1,64 @@
+const { test, expect } = require("@playwright/test");
+
+test('Handling JavaScript standard Alerts', async ({ page }) => {
+
+    await page.goto('https://the-internet.herokuapp.com/javascript_alerts');
+
+    page.on('dialog', async (dialog) => {
+        console.log(`Dialog message: ${dialog.message()}`);
+        expect(dialog.message()).toContain('I am a JS Alert');
+        await dialog.accept();
+    })
+
+    page.click('text=Click for JS Alert');
+    expect(page.getByText('You successfully clicked an alert')).toBeTruthy()
+
+})
+
+test('Handling JavaScript confirm Alerts', async ({ page }) => {
+
+    await page.goto('https://the-internet.herokuapp.com/javascript_alerts');
+
+    page.on('dialog', async (dialog) => {
+        console.log(`Dialog message: ${dialog.message()}`);
+        expect(dialog.message()).toContain('I am a JS Confirm');
+        await dialog.accept();
+    })
+
+    page.click('text=Click for JS Confirm');
+    expect(page.getByText('You clicked: Ok')).toBeTruthy()
+
+    page.click('text=Click for JS Confirm');
+    page.on('dialog', async (dialog) => {
+        console.log(`Dialog message: ${dialog.message()}`);
+        expect(dialog.message()).toContain('I am a JS Confirm');
+        await dialog.dismiss();
+    })
+    expect(page.getByText('You clicked: Cancel')).toBeTruthy()
+
+})
+
+test('Handling JavaScript prompt Alerts', async ({ page }) => {
+
+    await page.goto('https://the-internet.herokuapp.com/javascript_alerts');
+
+    const promptInput = 'Playwright Test';
+
+    page.on('dialog', async (dialog) => {
+        console.log(`Dialog message: ${dialog.message()}`);
+        expect(dialog.message()).toContain('I am a JS prompt');
+        await dialog.accept(promptInput);
+    })
+
+    page.click('text=Click for JS Prompt');
+    expect(page.getByText(`You entered: ${promptInput}`)).toBeTruthy()
+
+    page.click('text=Click for JS Prompt');
+    page.on('dialog', async (dialog) => {
+        console.log(`Dialog message: ${dialog.message()}`);
+        expect(dialog.message()).toContain('I am a JS prompt');
+        await dialog.dismiss();
+    })
+    expect(page.getByText('You entered: null')).toBeTruthy()
+
+})
